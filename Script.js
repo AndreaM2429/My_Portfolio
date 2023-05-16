9430b1f92f8d60a60e962f35d5253ebd85c2a0bf
gdvl777 <gdvl777@gmail.com>
2023-05-16 10:44:27 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -22,3 +22,13 @@ const move = document.getElementsByClassName('move');
 for (let i = 0; i < move.length; i += 1) {
   moveon(move[i]);
 }
+
+const projects = {
+  name: ['Tonic','Multi-post','Tonic','Multi-post'],
+  list: ['Canopy', 'Back End Dev', '2015'],
+  imag: ['Img/SnapshootPortfolio.png', '', '', '', '', ''],
+  desciption: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  tech: ['HTML', 'CSS', 'JavaScript'],
+  live: 'https://andream2429.github.io/Portfolio_setup_and_mobile_first/',
+  source: 'https://github.com/AndreaM2429/Portfolio_setup_and_mobile_first/'
+};
