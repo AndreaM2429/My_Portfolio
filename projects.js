3c23bea1174c09f2501defe1d14fd7a6b9515bb0
Andrea Manuel <andreamanuel2207@gmail.com>
2023-11-17 13:36:27 -0600

    Add new class names

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 const projectInfo = [
   {
     name: 'Conference Page',
-    list: ['Canopy', 'Front End Dev', '2023'],
+    list: ['Microverse', 'Front End Dev', '2023'],
     image: 'Img/Conference_Home.png',
     popImg: 'Img/Conference_Home.png',
     resume: 'This resume first',
@@ -12,12 +12,12 @@ const projectInfo = [
   },
   {
     name: 'To Do List',
-    list: ['Canopy', 'Front End Dev', '2023'],
+    list: ['Microverse', 'Front End Dev', '2023'],
     image: 'Img/Conference_Home.png',
     popImg: 'Img/Conference_Home.png',
     resume: 'This resume second',
     desciption: 'Is a project made with HTML, CSS and JS which purpose is to demonstrate my abilities mastering those development tools in a real case. This time the case is a conference page about comics and Superheros, the page count with two main sections that are Home and About page.',
-    tech: ['HTML', 'CSS', 'JavaScript'],
+    tech: ['HTML', 'CSS', 'JavaScript', 'Ruby on Rails'],
     live: 'https://andream2429.github.io/Capstone_project_Conference_page/',
     source: 'https://github.com/AndreaM2429/Capstone_project_Conference_page',
   },
@@ -31,16 +31,16 @@ for (let i = 0; i <= projectInfo.length; i += 1) {
   projectCard.innerHTML = `
     <img src="${projectInfo[i].image}" alt="${projectInfo[i].name} preview" class="projectImg">
     <div>
-      <h2>${projectInfo[i].name}</h2>
-      <ul>
-        ${projectInfo[i].list.map((item) => `<li>${item}</li>`).join('')}
+      <h2 class="card-title">${projectInfo[i].name}</h2>
+      <ul class="d-flex gap p0">
+        ${projectInfo[i].list.map((item) => `<li class="list">${item}</li>`).join('&#10026')}
       </ul>
-      <span>${projectInfo[i].resume}</span>
+      <span class="project-resume">${projectInfo[i].resume}</span>
       <div>
-      <ul>
-        ${projectInfo[i].tech.map((item) => `<li>${item}</li>`).join('')}
+      <ul class="d-flex gap p0">
+        ${projectInfo[i].tech.map((item) => `<li class="tech-list">${item}</li>`).join('')}
       </ul>
-      <button>See project</button>
+      <button class="see-project-btn">See project</button>
       </div>
     </div>
   `;
